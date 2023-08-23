b50034a5c42fb4850636ab1e1b1badd8afc1e7b1
Travis <travisjb0@gmail.com>
2023-08-22 19:58:56 -0700

    added starting point navigation system

--- eccomerce/src/Navigation/Nav.js
+++ eccomerce/src/Navigation/Nav.js
@@ -1,10 +1,37 @@
+import {FiHeart} from 'react-icons/fi'
+import {AiPOutlineShoppingCart, AiOutlineUSerAdd} from 'react-icons/ai'
+
 import React from 'react'
 import './Nav.css'
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <div className='nav-container'>
+        <input type='text' 
+        className='' 
+        placeholder='Enter your Search shoes.'
+        />
+
+        </div>
+
+       <div className='profile-container' >
+        <a href='#'>
+        <FiHeart className='nav-icons' />
+        </a>
+
+        <a href="">
+            <AiPOutlineShoppingCart className="nav-icons" />
+        </a>
+
+        <a href="">
+            <AiOutUserAdd className="nav-icons" />
+        </a>
+
+
+       </div>
+
+
+    </nav>
   )
 }
 
