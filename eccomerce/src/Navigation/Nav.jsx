94161cb44fb4ef35298da16f107850f4c50218f3
Travis <travisjb0@gmail.com>
2023-08-24 15:46:33 -0700

    Added navigation component

--- eccomerce/src/Navigation/Nav.jsx
+++ eccomerce/src/Navigation/Nav.jsx
@@ -1,35 +1,30 @@
 import {FiHeart} from 'react-icons/fi'
-
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
 
 import React from 'react'
 import './Nav.css'
 const Nav = () => {
   return (
     <nav>
-        <div className='nav-container'>
-        <input type='text' 
-        className='' 
-        placeholder='Enter your Search shoes.'
-        />
-
-        </div>
-
-       <div className='profile-container' >
+      <div className="nav-container">
+        <input type="text" className='search-input' placeholder='Enter your search shoes'  />
+      </div>
+      <div className="profile-container">
         <a href='#'>
-        <FiHeart className='nav-icons' />
+      <FiHeart className='nav-icons'/>
         </a>
 
-        <a href="#">
-            
-        </a>
+      <a href='#'>
+        <AiOutlineShoppingCart className="nav-icons"/>
+      </a>
 
-        <a href="#">
-         
-        </a>
+      <a href='#'>
+        <AiOutlineUserAdd className="nav-icons"/>
+      </a>
 
+     
 
-       </div>
-
+      </div>
 
     </nav>
   )
